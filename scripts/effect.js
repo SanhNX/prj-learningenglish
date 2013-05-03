ce4b9ac11ab1107c67da5965d25991206a3f440b
nittin990@gmail.com <nittin990@gmail.com@42cbe211-7b5d-10ee-ab1d-9236c30a0756>
2013-05-03 06:25:55 +0000

    [Design] Include effect

--- scripts/effect.js
+++ scripts/effect.js
@@ -2,17 +2,17 @@ $(function () {
     $(".menu-group").each(function () {
         $(this).hover(function () {
             $("#menu-popup").get(0).className = "menu-popup " + (this.id);
-            $("#menu-popup").css({overflow:"visible"}).stop().animate({width: 350}, 300);
-            $(".menu-popup-form." + this.id+" .menu-popup-list").css({right:350}).animate({right: 0}, 300);
+            $("#menu-popup").css({overflow: "visible"}).stop().animate({width: 350},1000,"easeOutQuint");
+            $(".menu-popup-form." + this.id + " .menu-popup-list").css({right: 350}).animate({right: 0},1000,"easeOutQuint");
 
         }, function () {
-            $("#menu-popup").css({overflow:"hidden"}).stop().animate({width: 0}, 300,function(){$(this).css({})});
+            $("#menu-popup").css({overflow: "hidden"}).stop().animate({width: 0});
         });
 
     });
     $("#menu-popup").hover(function () {
-        $(this).stop().css({overflow:"visible"}).animate({width: 350}, 300);
+        $(this).stop().animate({width: 350});
     }, function () {
-        $(this).stop().css({overflow:"hidden"}).animate({width: 0}, 300,function(){$(this).css({})});
+        $(this).stop().animate({width: 0});
     });
 });
