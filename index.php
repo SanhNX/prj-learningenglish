3f3f2af200308c707accb62013311fe72b4a6969
sanh23021987@gmail.com <sanh23021987@gmail.com@42cbe211-7b5d-10ee-ab1d-9236c30a0756>
2013-05-14 18:09:17 +0000

    - Add page index.php

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title></title>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	<script type="text/javascript" src="scripts/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="scripts/jquery-ui-1.10.2.min.js"></script>
+	<script type="text/javascript" src="scripts/jquery-colors-min.js"></script>
+	<script type="text/javascript" src="scripts/effect.js"></script>
+	
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id))
+            return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+<div class="page">
+<div class="menu">
+	<ul class="menu-list">
+		<li class="menu-item home"><a href="#">Trang chủ</a></li>
+		<li class="menu-item menu-group acc" id="acc"><a href="#">Tài khoản</a>
+
+			<div class="menu-arrow"></div>
+		</li>
+		<li class="menu-item menu-group cate" id="cate"><a href="#">Thể loại</a>
+
+			<div class="menu-arrow"></div>
+		</li>
+		<li class="menu-item menu-group day" id="day"><a href="#">BXH Ngày</a>
+
+			<div class="menu-arrow"></div>
+		</li>
+		<li class="menu-item menu-group week" id="week"><a href="#">BXH Tuần</a>
+
+			<div class="menu-arrow"></div>
+		</li>
+
+	</ul>
+	<div class="menu-popup" id="menu-popup">
+
+		<div class="menu-popup-form acc">
+			<ul class="menu-popup-list">
+				<li class="menu-popup-item">Đăng nhập</li>
+				<li class="menu-popup-item">Đăng kí</li>
+			</ul>
+		</div>
+		<div class="menu-popup-form cate">
+			<ul class="menu-popup-list">
+				<li class="menu-popup-item">Giải trí</li>
+				<li class="menu-popup-item">Âm nhạc</li>
+				<li class="menu-popup-item">Làm thế nào</li>
+				<li class="menu-popup-item">Khoa học</li>
+				<li class="menu-popup-item">Thế giới tự nhiên</li>
+				<li class="menu-popup-item">Thể thao</li>
+				<li class="menu-popup-item">Tâm lý người</li>
+				<li class="menu-popup-item">Máy tính</li>
+				<li class="menu-popup-item">Giao tiếp</li>
+				<li class="menu-popup-item">Dành cho trẻ em</li>
+				<li class="menu-popup-item">Thế giới</li>
+				<li class="menu-popup-item">Khi yêu</li>
+			</ul>
+		</div>
+		<div class="menu-popup-form day">
+			<ul class="menu-popup-list special">
+				<li class="menu-popup-item odd">Name1 2000</li>
+				<li class="menu-popup-item">Name2 4000</li>
+				<li class="menu-popup-item odd">Name3 2000</li>
+				<li class="menu-popup-item">Name4 4000</li>
+				<li class="menu-popup-item odd">Name5 2000</li>
+				<li class="menu-popup-item">Name6 4000</li>
+				<li class="menu-popup-item odd">Name7 2000</li>
+				<li class="menu-popup-item">Name8 4000</li>
+				<li class="menu-popup-item odd">Name9 2000</li>
+				<li class="menu-popup-item">Name10 4000</li>
+			</ul>
+		</div>
+		<div class="menu-popup-form week">
+			<ul class="menu-popup-list special">
+				<li class="menu-popup-item odd">Name1 2000</li>
+				<li class="menu-popup-item">Name2 4000</li>
+				<li class="menu-popup-item odd">Name3 2000</li>
+				<li class="menu-popup-item">Name4 4000</li>
+				<li class="menu-popup-item odd">Name5 2000</li>
+				<li class="menu-popup-item">Name6 4000</li>
+			</ul>
+		</div>
+	</div>
+
+</div>
+<div class="form">
+	<div class="form-head">
+		<span>Learning-English</span><span class="form-head-gray">Home</span>
+	</div>
+	<ul class="metro-list">
+		<li class="metro-item">
+			<div class="metro-bar">
+				<a class="metro-bar-wrapper" href="">
+					<div class=metro-title>It is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</a>
+			</div>
+		</li>
+		<li class="metro-item large">
+			<div class="metro-bar">
+				<a class="metro-bar-wrapper">
+					<div class=metro-title>It is a long established ntentIt is a long established fact that a reader
+						will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is fact that a reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</a>
+			</div>
+		</li>
+		<li class="metro-item">
+			<div class="metro-bar">
+				<a class="metro-bar-wrapper" href="">
+					<div class=metro-title></div>
+					<div class=metro-times></div>
+					<div class=metro-level></div>
+				</a>
+			</div>
+		</li>
+		<li class="metro-item medium">
+			<div class="metro-bar">
+				<a class="metro-bar-wrapper" href="">
+					<div class=metro-title>It is a long established fact that a reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</a>
+			</div>
+		</li>
+		<li class="metro-item">
+			<div class="metro-bar">
+				<a class="metro-bar-wrapper" href="">
+					<div class=metro-title>It is a long established fact that a reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</a>
+			</div>
+		</li>
+		<li class="metro-item large">
+			<div class="metro-bar">
+				<div class="metro-bar-wrapper">
+					<div class=metro-title>It is a long established fact that a ntentIt is a long established fact that
+						a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</div>
+			</div>
+		</li>
+		<li class="metro-item large">
+			<div class="metro-bar">
+				<div class="metro-bar-wrapper">
+					<div class=metro-title>It is a long established factntentIt is a long established fact that a reader
+						will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is that a reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</div>
+			</div>
+		</li>
+		<li class="metro-item">
+			<div class="metro-bar">
+				<div class="metro-bar-wrapper">
+					<div class=metro-title>It is a long established fact that a reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</div>
+			</div>
+		</li>
+		<li class="metro-item medium">
+			<div class="metro-bar">
+				<div class="metro-bar-wrapper">
+					<div class=metro-title>It is a long established fact ntentIt is a long established fact that a
+						reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is a long established fact that a reader will be distracted by the
+						readable contentIt is that a reader will be distracted by the
+						readable content
+					</div>
+					<div class=metro-times>200</div>
+					<div class=metro-level>999</div>
+				</div>
+			</div>
+		</li>
+		
+	</ul>
+	<div class="control-bar">
+		<div class=control-fb></div>
+		<div class=control-contact>
+			<div class="control-mess"></div>
+			<div class="control-mail"></div>
+		</div>
+		<div class=control-rank-recent>
+			<div class="control-rank-head"></div>
+			<ul class="control-rank-list">
+				<li class="control-rank-item">User 1</li>
+				<li class="control-rank-item">User 2</li>
+				<li class="control-rank-item">User 3</li>
+				<li class="control-rank-item">User 4</li>
+			</ul>
+		</div>
+	</div>
+	<div class="fb-bar">
+		<div class="fb-like-box" data-href="http://www.facebook.com/voalearningenglish" data-width="1040" data-height="210" data-show-faces="true" data-stream="false" data-header="false"></div>
+	</div>
+</div>
+<div class="popup disable">
+	<div class="popup-form login">
+		<div class="popup-wrapper">
+			<div class="login-avt"></div>
+			<div class="login-info">
+				<div class="login-info-title">
+					Đăng nhập
+				</div>
+				<div class="login-info-row"><span class="login-info-label">Tên tài khoản:</span>
+					<input class="login-info-input" type="text" value=""/>
+				</div>
+				<div class="login-info-row"><span class="login-info-label">Mật khẩu:</span>
+					<input class="login-info-input" type="password" value=""/>
+				</div>
+				<div class="login-info-row large">
+					<a class="login-info-link">Quên mật khẩu?</a>
+					<a class="login-info-button">Đăng nhập</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</body>
+</html>
