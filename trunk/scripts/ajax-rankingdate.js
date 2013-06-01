e98f0f4f3e374f12857dadb480229c5103c3043c
thanhbr39@gmail.com <thanhbr39@gmail.com@42cbe211-7b5d-10ee-ab1d-9236c30a0756>
2013-06-01 05:15:29 +0000

    code index 01-06

--- trunk/scripts/ajax-rankingdate.js
+++ trunk/scripts/ajax-rankingdate.js
@@ -4,24 +4,25 @@ $(document).ready(function() {
     var dd = today.getDate();
     var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
-
-    var currentDate = '<i class="menu-popup-icon calendar-date"></i>\n\<span>' + mm + '/' + dd + '/' + yyyy + '</span>\n\<input id="currentdate" value="' + yyyy + '-' + mm + '-' + dd + '" type="hidden">';
-
+    var currentDate = '<i class="menu-popup-icon calendar-date" id="currentdateIcon"></i><input id="currentdate"  class="date-pick" value="' + yyyy + '-' + mm + '-' + dd + '">';
     $("#popup-current-ranking-date").html(currentDate);
-
     var paramdatesearch = yyyy + '-' + mm + '-' + dd;
     getRankingByDate(paramdatesearch, "1");
-
-    $('.menu-popup-calendar-prev').click(function() {
-    });
-
-
     // week
 
     // to do start date va end date
     var startdate = "2013-01-13";
     var endtdate = "2013-05-26";
     getRankingByDate(startdate, endtdate);
+    
+    
+    
+    
+    // change date
+    $('#currentdateIcon').focus(function() {
+        alert("asdasd");
+    });
+
 });
 function getRankingByDate($paramDate1, $paramDate2) {
     var str_string = 'date1=' + $paramDate1 + '&date2=' + $paramDate2;
