e33e705b539e65f263a73ab1d79de0790444baae
sanh23021987@gmail.com <sanh23021987@gmail.com@42cbe211-7b5d-10ee-ab1d-9236c30a0756>
2013-06-11 12:38:24 +0000

    - fix css

--- trunk/profile.php
+++ trunk/profile.php
@@ -16,6 +16,9 @@ include 'BLL/activityHistoryBLL.php';
         <link href="css/date-picker.css" rel="stylesheet" type="text/css"/>
         <link href="css/jquery.mCustomScrollbar.css" rel="stylesheet" type="text/css"/>
         <script type="text/javascript" src="scripts/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="scripts/jquery.cookie.js"></script>
+        <script type="text/javascript" src="scripts/facebookAPI.js"></script>
+        <script type="text/javascript" src="scripts/ajax-userprocess.js"></script>
         <script type="text/javascript" src="scripts/jquery-ui-1.10.2.min.js"></script>
         <script type="text/javascript" src="scripts/jquery-colors-min.js"></script>
         <script type="text/javascript" src="scripts/date.js"></script>
