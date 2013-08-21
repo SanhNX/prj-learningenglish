8a12bcf7d255b1b7f4f1cecabf3ea54a8c4daa08
sanh23021987@gmail.com <sanh23021987@gmail.com@42cbe211-7b5d-10ee-ab1d-9236c30a0756>
2013-08-21 17:57:50 +0000

    update admin category

---
+++ BLL/adminChooseCategoryBll.php
@@ -0,0 +1,22 @@
+﻿<?php
+include '../DAO/connection.php';
+include '../DTO/object.php';
+include '../BLL/categoryBll.php';
+
+$flag = $_POST['flag'];
+$chooseValue = $_POST['chooseValue'];
+$categoryList = null;
+$optionList = '';
+if($flag == 'chooseCate'){
+    if($chooseValue == 'E'){
+        $categoryList = getAllCategoryForEveryone();
+    } else {
+        $categoryList = getAllCategoryForKids();
+    }
+    for ($i = 0; $i < count($categoryList); $i++) {
+        $item = $categoryList[$i];
+        $optionList = $optionList . '<option value="'. $item->id .'">'. $item->name .'</option>';
+    }
+    echo $optionList;
+}
+?>
